b72f8f51d0dbf4a4bed91439538fe8cca98989e0
Alex King <alexking@me.com>
2014-03-27 21:35:59 -0400

    Simple gabled roofs

--- src/client/objects/ObjectManager.js
+++ src/client/objects/ObjectManager.js
@@ -126,57 +126,61 @@
 					// to do roof shortcuts
 					if (shape.curves.length === 4) {
 
-						// Roof geometry
-						var roof = new THREE.Geometry();
+						// Check if it's a gabled roof
+						if (properties.roof.shape === "gabled") {
 
-						// Grab the points from the shape
-						var points = shape.extractPoints();
+							// Roof geometry
+							var roof = new THREE.Geometry();
 
-						// Figure out the roof height
-						var roofHeight = -(height / 2);
+							// Grab the points from the shape
+							var points = shape.extractPoints();
 
-						// Figure out the center points
-						var center1 = points.shape[0].clone().lerp(points.shape[1], 0.5);
-						var center2 = points.shape[2].clone().lerp(points.shape[3], 0.5);
+							// Figure out the roof height
+							var roofHeight = -(height / 2);
 
-						// Create the vertices
-						var vertices = [
-							new THREE.Vector3(points.shape[0].x, points.shape[0].y, 0),
-							new THREE.Vector3(center1.x,         center1.y,         roofHeight),
-							new THREE.Vector3(points.shape[1].x, points.shape[1].y, 0),
-							new THREE.Vector3(points.shape[2].x, points.shape[2].y, 0),
-							new THREE.Vector3(center2.x,         center2.y,         roofHeight),
-							new THREE.Vector3(points.shape[3].x, points.shape[3].y, 0),
-						];
+							// Figure out the center points
+							var center1 = points.shape[0].clone().lerp(points.shape[1], 0.5);
+							var center2 = points.shape[2].clone().lerp(points.shape[3], 0.5);
 
-						// Ensure the points are clockwise
-						var clockwise = THREE.Shape.Utils.isClockWise(vertices);
-						if (!clockwise) {
-							vertices = vertices.reverse();
+							// Create the vertices
+							var vertices = [
+								new THREE.Vector3(points.shape[0].x, points.shape[0].y, 0),
+								new THREE.Vector3(center1.x,         center1.y,         roofHeight),
+								new THREE.Vector3(points.shape[1].x, points.shape[1].y, 0),
+								new THREE.Vector3(points.shape[2].x, points.shape[2].y, 0),
+								new THREE.Vector3(center2.x,         center2.y,         roofHeight),
+								new THREE.Vector3(points.shape[3].x, points.shape[3].y, 0),
+							];
+
+							// Ensure the points are clockwise
+							var clockwise = THREE.Shape.Utils.isClockWise(vertices);
+							if (!clockwise) {
+								vertices = vertices.reverse();
+							}
+
+							roof.vertices = vertices;
+
+							// Side 1
+							roof.faces.push(new THREE.Face3(3, 4, 1));
+							roof.faces.push(new THREE.Face3(3, 1, 2));
+
+							// Front/Back
+							roof.faces.push(new THREE.Face3(4, 3, 5));
+							roof.faces.push(new THREE.Face3(1, 0, 2));
+
+							// Side 2
+							roof.faces.push(new THREE.Face3(0, 1, 4));
+							roof.faces.push(new THREE.Face3(0, 4, 5));
+
+							// We aren't generating actual UVs, but the exporter needs
+							// some placeholder points
+							_.each(roof.faces, function() {
+								roof.faceVertexUvs[0].push([false, false, false]);
+							});
+
+							// Add to the building geometry
+							THREE.GeometryUtils.merge(geom, roof);
 						}
-
-						roof.vertices = vertices;
-
-						// Side 1
-						roof.faces.push(new THREE.Face3(3, 4, 1));
-						roof.faces.push(new THREE.Face3(3, 1, 2));
-
-						// Front/Back
-						roof.faces.push(new THREE.Face3(4, 3, 5));
-						roof.faces.push(new THREE.Face3(1, 0, 2));
-
-						// Side 2
-						roof.faces.push(new THREE.Face3(0, 1, 4));
-						roof.faces.push(new THREE.Face3(0, 4, 5));
-
-						// We aren't generating actual UVs, but the exporter needs
-						// some placeholder points
-						_.each(roof.faces, function() {
-							roof.faceVertexUvs[0].push([false, false, false]);
-						});
-
-						// Add to the building geometry
-						THREE.GeometryUtils.merge(geom, roof);
 
 					}
 
