06b6ee940d8d5598eb742d2b8d8ff08eb379309f
Robin Hawkes <rob@rawkes.com>
2016-04-19 08:43:41 +0100

    Enabled full demo

--- examples/basic/main.js
+++ examples/basic/main.js
@@ -41,43 +41,43 @@ var imageTileLayer = VIZI.imageTileLayer('http://{s}.basemaps.cartocdn.com/light
 // }).addTo(world);
 
 // Building and roads from Mapzen (polygons and linestrings)
-// var topoJSONTileLayer = VIZI.topoJSONTileLayer('https://vector.mapzen.com/osm/buildings,roads/{z}/{x}/{y}.topojson?api_key=vector-tiles-NT5Emiw', {
-//   interactive: false,
-//   style: function(feature) {
-//     var height;
-//
-//     if (feature.properties.height) {
-//       height = feature.properties.height;
-//     } else {
-//       height = 10 + Math.random() * 10;
-//     }
-//
-//     return {
-//       height: height,
-//       lineColor: '#f7c616',
-//       lineWidth: 1,
-//       lineTransparent: true,
-//       lineOpacity: 0.2,
-//       lineBlending: THREE.AdditiveBlending,
-//       lineRenderOrder: 2
-//     };
-//   },
-//   filter: function(feature) {
-//     // Don't show points
-//     return feature.geometry.type !== 'Point';
-//   },
-//   // onEachFeature: function(feature, layer) {
-//   //   if (feature.geometry.type !== 'Polygon') {
-//   //     return;
-//   //   }
-//   //
-//   //   // Make polygons clickable
-//   //   layer.on('click', function(layer, point2d, point3d, intersects) {
-//   //     console.log(layer, point2d, point3d, intersects);
-//   //   });
-//   // },
-//   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://whosonfirst.mapzen.com#License">Who\'s On First</a>.'
-// }).addTo(world);
+var topoJSONTileLayer = VIZI.topoJSONTileLayer('https://vector.mapzen.com/osm/buildings,roads/{z}/{x}/{y}.topojson?api_key=vector-tiles-NT5Emiw', {
+  interactive: false,
+  style: function(feature) {
+    var height;
+
+    if (feature.properties.height) {
+      height = feature.properties.height;
+    } else {
+      height = 10 + Math.random() * 10;
+    }
+
+    return {
+      height: height,
+      lineColor: '#f7c616',
+      lineWidth: 1,
+      lineTransparent: true,
+      lineOpacity: 0.2,
+      lineBlending: THREE.AdditiveBlending,
+      lineRenderOrder: 2
+    };
+  },
+  filter: function(feature) {
+    // Don't show points
+    return feature.geometry.type !== 'Point';
+  },
+  // onEachFeature: function(feature, layer) {
+  //   if (feature.geometry.type !== 'Polygon') {
+  //     return;
+  //   }
+  //
+  //   // Make polygons clickable
+  //   layer.on('click', function(layer, point2d, point3d, intersects) {
+  //     console.log(layer, point2d, point3d, intersects);
+  //   });
+  // },
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://whosonfirst.mapzen.com#License">Who\'s On First</a>.'
+}).addTo(world);
 
 // Just buildings from Mapzen (polygons)
 // var topoJSONTileLayer = VIZI.topoJSONTileLayer('https://vector.mapzen.com/osm/buildings/{z}/{x}/{y}.topojson?api_key=vector-tiles-NT5Emiw', {
