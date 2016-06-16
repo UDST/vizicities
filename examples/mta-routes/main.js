97912ba8421705098853392265a6662863f6b752
Robin Hawkes <rob@rawkes.com>
2016-06-16 09:46:59 +0100

    Added MTA route example

---
+++ examples/mta-routes/main.js
@@ -0,0 +1,35 @@
+// Manhattan
+var coords = [40.739940, -73.988801];
+
+var world = VIZI.world('world', {
+  skybox: false,
+  postProcessing: false
+}).setView(coords);
+
+// world._environment._skybox.setInclination(0.3);
+
+// Add controls
+VIZI.Controls.orbit().addTo(world);
+
+VIZI.imageTileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+}).addTo(world);
+
+// MTA routes
+VIZI.geoJSONLayer('https://cdn.rawgit.com/robhawkes/0b08e6e60fd329bf2ef342c1122b9d43/raw/02954a741abd7d852c0cecb24a71252a74eac154/mta-routes-simplified.geojson', {
+  output: true,
+  interactive: false,
+  style: function(feature) {
+    var colour = (feature.properties.color) ? '#' + feature.properties.color : '#ffffff';
+
+    return {
+      lineColor: colour,
+      lineWidth: 1.5,
+      // lineTransparent: true,
+      // lineOpacity: 0.2,
+      // lineBlending: THREE.AdditiveBlending,
+      lineRenderOrder: 2
+    };
+  },
+  attribution: '&copy; NYC MTA.'
+}).addTo(world);
