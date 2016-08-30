703b23feaf78cf4dfaf4a2e9c1d8f2486874bbca
Robin Hawkes <rob.hawkes@gmail.com>
2016-08-30 16:59:16 +0100

    Updated basic example to show off promises

--- examples/basic/main.js
+++ examples/basic/main.js
@@ -12,7 +12,9 @@ VIZI.Controls.orbit().addTo(world);
 // CartoDB basemap
 VIZI.imageTileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-}).addTo(world);
+}).addTo(world).then(() => {
+  console.log('Added image tile layer to world');
+});;
 
 // Buildings from Mapzen
 VIZI.topoJSONTileLayer('https://vector.mapzen.com/osm/buildings/{z}/{x}/{y}.topojson?api_key=vector-tiles-NT5Emiw', {
@@ -36,5 +38,5 @@ VIZI.topoJSONTileLayer('https://vector.mapzen.com/osm/buildings/{z}/{x}/{y}.topo
   },
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://whosonfirst.mapzen.com#License">Who\'s On First</a>.'
 }).addTo(world).then(() => {
-  console.log('Added to world');
+  console.log('Added TopoJSON layer to world');
 });
